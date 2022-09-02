6e693caa3de191f1535d82e8d93c8ef94d9dc8e8
sajeev.srinivasan <sajeev.srinivasan@thoughtworks.com>
2022-09-02 18:39:05 +0530

    Add. Add random number generator for target number in view controller

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,20 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var currentValue = 0
+    var target = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let value = slider.value.rounded()
+        currentValue = Int(value)
+        startNewRound()
     }
 
     @IBAction func showAlert() {
         
-        let alert = UIAlertController(title: "Hey Sajeev", message: "This is BullsEye!!", preferredStyle: .alert)
+        let message = "The value of the slider is \(currentValue)"
+        let alert = UIAlertController(title: "Bull's Eye", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Close", style: .default, handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
-    @IBAction func sliderMoved(_ slider: UISlider) {
-        print("Value of the slider: \(slider.value)")
+    func startNewRound(){
+        target = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        targetLabel.text = String(target)
     }
 }
