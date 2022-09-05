0deeb60d3195a3c8843f15393a428942d33c681d
sajeev.srinivasan <sajeev.srinivasan@thoughtworks.com>
2022-09-05 22:29:14 +0530

    Add. Styling for labels, buttons and slider

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Sajeev S on 05/09/22.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
